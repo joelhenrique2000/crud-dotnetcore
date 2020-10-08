753967fa6a9b7f88613428be196d17d060b78879
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-08 13:16:08 -0300

    instalando o Dapper, SqlConnection e adicionando o Model

---
+++ Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_exemplo.Models
+{
+    public class Pessoa
+    {
+        public long Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Repository/PessoaRepository.cs
@@ -0,0 +1,32 @@
+﻿using pessoa_exemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace pessoa_exemplo.Repository
+{
+    public class PessoaRepository
+    {
+        private readonly string _connectionString;
+
+        public PessoaRepository() {
+            _connectionString = "Server=localhost\\SQLEXPRESS; Database = ESTUDO1; Trusted_Connection = true";
+        }
+
+        // public PessoaRepository(IConfiguration configuration)
+        //{
+        //    _connectionString = configuration.GetConnectionString("PessoaDataServer");
+        // }
+
+        public IEnumerable<Pessoa> ListAll() {
+            using (var connection = new SqlConnection(""))
+                using var connection = new SqlConnection(_connectionString);
+
+            var pessoaData = connection.Query<Pessoa>("SELECT * FROM PESSOA;");
+
+            return pessoaData;
+        }
+    }
+}
